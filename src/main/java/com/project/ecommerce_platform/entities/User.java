a9a01e5498ecf7190a9aeb3a24b29052f45af841
Dara <davidfamoyegun@gmail.com>
2024-06-18 12:14:07 +0100

    SignUp and GetAllUser Endpoint created and tested

--- src/main/java/com/project/ecommerce_platform/entities/User.java
+++ src/main/java/com/project/ecommerce_platform/entities/User.java
@@ -2,35 +2,30 @@ package com.project.ecommerce_platform.entities;
 
 import com.project.ecommerce_platform.utilities.Roles;
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Setter
 @ToString
 @Entity
-@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @Column(name =  "email", nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(nullable = false)
     private String password;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(nullable = false)
     private String first_name;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(nullable = false)
     private String last_name;
 
     @Enumerated(EnumType.STRING)
