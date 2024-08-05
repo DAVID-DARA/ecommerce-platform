3ef107d15f8e8cbc6d68129038c16507a125c6ba
DAVID-DARA <davidfamoyegun1@gmail.com>
2024-08-05 19:20:03 +0100

    Allowed live domain to send request Cors and Security Config

--- src/main/java/com/project/ecommerce_platform/configurations/CorsConfig.java
+++ src/main/java/com/project/ecommerce_platform/configurations/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/user/signup") // Allow all paths
                         .allowedOrigins("http://localhost:5173/**") // Specify the allowed origins
+                        .allowedOrigins("https://ecommerce-ui-iota.vercel.app/**") // Specify the allowed origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify the allowed methods
                         .allowedHeaders("*") // Specify the allowed headers
                         .allowCredentials(true); // Allow credentials (e.g., cookies)
